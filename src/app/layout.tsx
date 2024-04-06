8dfcace6a603da1a353bb30866b1c1c534f29ea5
Raaz <21ketanraaz@gmail.com>
2024-04-06 16:59:38 +0530

    Styling the sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex">
+          <div className="bg-[202123] max-2-xs h-screen overflow-y-auto md:min-w-[20rem]">
           <SideBar/>
+
+          </div>
           {/* {Sidebar} */}
           {/* client provider-Notification */}
      <div className=" bg-[#343541] flex-1 ">
