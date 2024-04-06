6b9f6916c02430f495dd2f9e073e9d3dbc652ffb
Raaz <21ketanraaz@gmail.com>
2024-04-06 16:27:54 +0530

    Adding HeroIcons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
-function HomePage(){
+function HomePage() {
   return (
     <div className="flex  flex-col items-center h-screen px-2 justify-center text-white">
       <h1 className=" text-5xl font-bold mb-20">ChatGpt</h1>
       <div>
-        
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">sun icon</div>
+          <h1>Examples</h1>
+        </div>
+        <div className=" space-y-2">
+          <p className="infoText">Explain Something to me</p>
+          <p className="infoText ">
+            "What is the difference between a crocodile and an alligator?"
+          </p>
+          <p className="infoText">What is the color of the sun</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 export default HomePage;
