ceb29ed26c6ac7081b3a6a55578d9b363c152b8f
Raaz <21ketanraaz@gmail.com>
2024-04-06 16:21:17 +0530

    Getting Started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 function HomePage(){
   return (
-    
+    <div className="flex  flex-col items-center h-screen px-2 justify-center text-white">
+      <h1 className=" text-5xl font-bold mb-20">ChatGpt</h1>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 export default HomePage;
