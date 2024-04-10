87a3052d61bf4145189ea345cee1c415ed5dceed
Raaz <21ketanraaz@gmail.com>
2024-04-10 23:50:50 +0530

    Fixing  a major .env bug

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,9 +1,20 @@
-// import React from 'react'
+'use client'
+import { signIn } from "next-auth/react"
+import Image from "next/image"
 
 function Login() {
+  
   return (
-    <div>
-      Login
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+      <Image
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/ChatGPT-Logo.png/1200px-ChatGPT-Logo.png"
+      width={300}
+        height={300}
+        alt="ChatGPT Logo"
+      />
+      <button onClick={()=>signIn('google')} className="text-white font-bold text-3xl animate-pulse">
+        Sign In to use ChatGpt
+      </button>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default async function RootLayout({
 }>) 
 {
 
+
 const session = await getServerSession(authOptions);
 
   return (
