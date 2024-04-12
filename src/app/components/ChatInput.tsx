3ab0244432ac79824d0896d8a9a0841d6b95b65a
Raaz <21ketanraaz@gmail.com>
2024-04-12 21:17:08 +0530

    Adding the ChatInput Section

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
+import { useState } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/16/solid";
+import { useSession } from "next-auth/react";
 
-function ChatInput() {
+type Props = {
+    chatId:string;
+}
+function ChatInput({chatId}:Props) {
+    const [prompt, setPrompt] = useState("");
+    const {data:session} = useSession();
   return (
-    <div>
-      ChatInput
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm focus:outline-none">
+      <form className="p-5 space-x-5 flex">
+        <input 
+        className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+        disabled = {!session}
+        value={prompt}
+        onChange={(e)=>setPrompt(e.target.value)}
+        type="text" placeholder="Type a message ..."/>
+        <button 
+        disabled={!session || !prompt}
+        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+        type="submit">
+            <PaperAirplaneIcon className="h-4 w-4 -rotate-45"/>
+        </button>
+      </form>
+      <div>
+        {/* {ModalSection} */}
+      </div>
     </div>
   )
 }
