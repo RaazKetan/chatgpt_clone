0ec630198ddd200d11315a1122c0ad0dcf6e23f8
Raaz <21ketanraaz@gmail.com>
2024-04-12 21:31:29 +0530

    Message Digest

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { PaperAirplaneIcon } from "@heroicons/react/16/solid";
 import { useSession } from "next-auth/react";
+import { Message } from "postcss";
+import { serverTimestamp } from "firebase/firestore";
 
 type Props = {
     chatId:string;
@@ -9,9 +11,31 @@ type Props = {
 function ChatInput({chatId}:Props) {
     const [prompt, setPrompt] = useState("");
     const {data:session} = useSession();
+
+    const sendMessage = async(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        if(!prompt)
+            return;
+
+        const input = prompt.trim();
+        setPrompt("");
+
+        const message:Message = {
+            text:input,
+            createdAt: serverTimestamp(),
+            user:{
+                _id:session?.user?.email!,
+                name:session?.user?.name!,
+                avatar:session?.user?.image! ||  `https://ui-avatars.com/api/?name=${session?.user?.name}&background=random&color=fff` ,
+            }
+        }
+    }
+
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm focus:outline-none">
-      <form className="p-5 space-x-5 flex">
+      <form
+      onSubmit={sendMessage}
+      className="p-5 space-x-5 flex">
         <input 
         className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
         disabled = {!session}
