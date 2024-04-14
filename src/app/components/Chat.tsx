2deca23b380e34f6cfc826d47db5ff7eaaf043d5
Raaz <21ketanraaz@gmail.com>
2024-04-14 14:33:37 +0530

    Adding Chat Components

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,11 +1,42 @@
+"use client";
+import { collection, or, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../firebase";
+import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+
 type Props = {
     chatId:string;
 }
 
 function Chat({chatId}:Props) {
+  const { data: session } = useSession();
+  const [message] = useCollection(
+    session && query(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      orderBy("createdAt", "asc")
+    )
+  )
   return (
-    <div className="flex-1">
-      Chat
+    <div className="flex-1 overflow-y-auto overflow-x-hidden" >
+      {message?.empty &&
+      <>
+      <p className="mt-10 text-center text-white">Type a prompt in below to get started</p>
+      <ArrowDownCircleIcon className="h-10 w-10 mx-auto text-white animate-bounce mt-5" />
+      </>
+      }
+      {message?.docs.map((message) => (
+      <Message key={message.id} message={message.data()} />
+      ))  
+      }
     </div>
   )
 }
---
+++ src/app/components/Message.tsx
@@ -0,0 +1,22 @@
+import { DocumentData } from 'firebase-admin/firestore'
+import React from 'react'
+
+type Props = {
+    message: DocumentData
+}
+
+function Message({message}: Props) {
+const isChatGpt = message.user.name === 'ChatGPT';
+  return (
+    <div className={`py-5 text-white ${isChatGpt && "bg-[#434654]" }`}>
+        <div className='flex space-x-5 px-10 max-w-2xl mx-auto'>
+        <img src={message.user.avatar} alt={message.user.name} className='h-8 w-8' />
+      <p className='pt-1 text-sm'>
+        {message.text}
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default Message
